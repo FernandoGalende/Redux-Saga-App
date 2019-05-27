b162545c605e463ce50152870eee60127dab1ada
FernandoGalende <fernandogalende@gmail.com>
2019-05-27 16:12:43 +0200

    implentets views and presentational components

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -1,11 +1,14 @@
 import {takeEvery, takeLatest, take, call, put, fork} from 'redux-saga/effects';
 import * as actions from '../actions/users';
-import * as api from '../api/users'
+import * as api from '../api/users';
 
 function* getUsers(){
 	try{
-    const result = yield call(api.getUsers)
-    console.log('result: ', result)
+    const { data } = yield call(api.getUsers)
+    console.log('data: ', data)
+    yield put(actions.getUsersSuccess({
+      items: data.data
+    }))
 	}catch(e){
     console.error('error: ', e)
 	}
